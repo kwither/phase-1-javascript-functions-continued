be6e208a2d56cbb2cfa5e9312997098d64ac4163
Kevin Wither <kvn.wither@gmail.com>
2022-05-01 12:52:21 -0600

    commit tree bd6155e55b5348d22e5859c4d79a56160d2309f5

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(toDo = "go to the office") {
+    return `This Monday, I will ${toDo}.`
+}
+
+
+
+function wrapAdjective(result = "*" && "||") {
+    return function(greeting, emphatic = "a hard worker") {
+        return `${greeting = "You are"} ${result}${emphatic}${result}!`;
+    }
+}
